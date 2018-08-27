9044879b000763c926a90e19525dd3bf2b5b2170
The Combed Thunderclap <clappedthunder@gmail.com>
2018-08-27 14:30:33 +0100

    Remove batch_first=True from LSTM; add keras script and both notebooks

--- pytorch-stock-predictor-rnn.py
+++ pytorch-stock-predictor-rnn.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python2.7
 
-# Stock Predictor
-# Adapted from Keras version available at https://www.superdatascience.com/deep-learning/
-
-# Part 1 - Data Preprocessing
+# Pytorch RNN Stock Predictor
+# 
+# This is a Pytorch version of the original Keras LSTM Stock Predictor provided by [Super DataScience](https://www.superdatascience.com/deep-learning/). The Keras version is available elsewhere in this repo.
+# This pytorch script also has an accompanying notebook.
+# 
+# The pytorch version differs from the original in that it uses an RNN rather than LSTM.
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch.nn as nn
+import torch
+from torch.autograd import Variable
 
 # Globals
 
@@ -14,6 +21,11 @@ INPUT_SIZE = 60
 HIDDEN_SIZE = 64
 NUM_LAYERS = 2
 OUTPUT_SIZE = 1
+
+# Hyper parameters
+
+learning_rate = 0.001
+num_epochs = 50
 
 # Importing the training set
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
@@ -35,11 +47,6 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
 
-import torch.nn as nn
-import torch
-from torch.autograd import Variable
-
-# Part 2 - Building the RNN
 class RNN(nn.Module):
     def __init__(self, i_size, h_size, n_layers, o_size):
         super(RNN, self).__init__()
@@ -47,8 +54,7 @@ class RNN(nn.Module):
         self.rnn = nn.LSTM(
             input_size=i_size,
             hidden_size=h_size,
-            num_layers=n_layers,
-            batch_first=True,
+            num_layers=n_layers
         )
         self.out = nn.Linear(h_size, o_size)
 
@@ -61,30 +67,25 @@ class RNN(nn.Module):
 
         return outs, hidden_state
 
-
-
 rnn = RNN(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE)
 
-optimiser = torch.optim.Adam(rnn.parameters(), lr=0.001)
-loss_func = nn.MSELoss()
+optimiser = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
+criterion = nn.MSELoss()
 
 hidden_state = None
 
-for epoch in range(10):
+for epoch in range(num_epochs):
     inputs = Variable(torch.from_numpy(X_train).float())
     labels = Variable(torch.from_numpy(y_train).float())
 
     output, hidden_state = rnn(inputs, hidden_state) 
-#    hidden_state = Variable(hidden_state)
 
-    loss = loss_func(output.view(-1), labels)
+    loss = criterion(output.view(-1), labels)
     optimiser.zero_grad()
     loss.backward(retain_graph=True)                     # back propagation
     optimiser.step()                                     # update the parameters
     
     print('epoch {}, loss {}'.format(epoch,loss.item()))
-
-# Part 3 - Making the predictions and visualising the results
 
 # Getting the real stock price of 2017
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
@@ -101,9 +102,22 @@ for i in range(INPUT_SIZE, 80):
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
-test_inputs = Variable(torch.from_numpy(X_test).float())
+X_train_X_test = np.concatenate((X_train, X_test),axis=0)
 hidden_state = None
+test_inputs = Variable(torch.from_numpy(X_train_X_test).float())
 predicted_stock_price, b = rnn(test_inputs, hidden_state)
-predicted_stock_price = np.reshape(predicted_stock_price.detach().numpy(), (X_test.shape[0], 1))
+predicted_stock_price = np.reshape(predicted_stock_price.detach().numpy(), (test_inputs.shape[0], 1))
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+real_stock_price_all = np.concatenate((training_set[INPUT_SIZE:], real_stock_price))
+
+# Visualising the results
+plt.figure(1, figsize=(12, 5))
+plt.plot(real_stock_price_all, color = 'red', label = 'Real')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Pred')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
 
